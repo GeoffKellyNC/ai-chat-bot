e4d4cd6e05356c0d300569dcdfbaa19c2153da42
Geoff Kelly <geoffkellync@gmail.com>
2023-01-10 22:38:48 -0500

    set up server.js file to run on port 3030

--- server.js
+++ server.js
@@ -4,3 +4,42 @@ const app = express();
 const cors = require('cors');
 const {berry} = require('./twitch/berry')
 const os = require('os');
+
+
+
+
+const userArg = process.argv[2] 
+
+const DEBUG = userArg === 'debug' ? true : false
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Server is up and running</h1>')
+})
+
+
+
+
+
+
+for (const key in interfaces) {
+    for (const address of interfaces[key]) {
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.push(address.address);
+      }
+    }
+  }
+
+
+app.listen(port, () => {
+    DEBUG ? console.log('DEBUGGING IS ON!!!') : null
+    console.log(`Server is running on port ${port}....`);
+    console.log(`Server is also running on EXTERNAL ${addresses[0]}:${port}`)
+});
+
+
+module.exports = app;
